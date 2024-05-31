6567ee54348b161a8407f6d9801832ebec3fdb2c
Caio Marx <icaiomarxm@gmail.com>
2024-05-31 08:18:18 -0300

    feat: adding from_secret command

---
+++ otp_generator/cli/commands.py
@@ -0,0 +1,9 @@
+import click
+from otp_generator.two_factor.generate_otp import generate_totp_from_secret
+
+@click.command()
+@click.option('--secret', 'secret', help='', required=True, type=str)
+def from_secret(
+    secret: str
+):
+    return generate_totp_from_secret(secret=secret)
